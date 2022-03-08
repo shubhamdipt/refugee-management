2084777a1bc940d9c7d7a414d09e18898cd6c38f
Shubham Dipt <shubham.dipt@gmail.com>
2022-03-09 00:54:11 +0100

    [feature] refugee and volunteer app added

---
+++ refugee/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RefugeeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'refugee'
--- refugee_management/settings.py
+++ refugee_management/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     # Apps
     "accounts",
     "frontend",
+    "volunteer",
+    "refugee",
 ]
 
 MIDDLEWARE = [
