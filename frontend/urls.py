19f55de0c22f2db1b1758dedac8ce474b4ca8aa4
Shubham Dipt <shubham.dipt@gmail.com>
2022-03-09 00:51:17 +0100

    [feature] data privacy and footer added

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from frontend.views import home
+from frontend.views import data_privacy, home
 
 app_name = "frontend"
 urlpatterns = [
     path("", home, name="home"),
+    path("data-privacy", data_privacy, name="data_privacy"),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -5,3 +5,9 @@ def home(request):
     """View for the home page."""
     ctx = {}
     return render(request, "frontend/home.html", ctx)
+
+
+def data_privacy(request):
+    """View for the data privacy page."""
+    ctx = {}
+    return render(request, "frontend/data_privacy.html", ctx)
